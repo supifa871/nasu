5da0775cccb8d92cf39850dbd5386beaf1ee9209
supifa871 <92263287+supifa871@users.noreply.github.com>
2021-10-11 00:15:13 +0900

    Create README.m

---
+++ README.m
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <base target="_top">
+  </head>
+  <body>
+    <script>
+
+var pw;
+
+pw = prompt("パスワードを入れて下さい。","");
+
+if (pw == "YouTube"){
+ "https://www.google.co.jp/";
+}else{
+  alert("パスワードが違います！");
+}
+
+</script>
+</SCRIPT>
+  </body>
+</html>
